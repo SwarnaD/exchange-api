3f7b73a3840179149b23b0896a3aaf8ffefd4d84
Swarnajyoti Datta <swarna.datta01@gmail.com>
2021-10-24 12:51:49 -0400

    added raw rates response and bad route response

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,61 +1,97 @@
 class MainController < ApplicationController
+  # calculate conversion of currency
   def calculate
-    # calculate conversion of currency
     response = {}
     from = params[:from_currency].upcase
     to = params[:to_currency].upcase
+
+    conversion_rates = Rails.cache.read('exchange_conversion_rates')
+
+    # no rate data
+    if conversion_rates.nil?
+      response['error'] = 'no data'
+      response['success'] = 'false'
+      render json: response.to_json, status: (:service_unavailable) and return
+    end
 
     # invalid amount
     begin
       amount = BigDecimal(params[:amount])
     rescue ArgumentError
-      response['status'] = 'invalid amount'
-      render json: response.to_json and return
-    end
-  
-    conversion_rates = Rails.cache.read('exchange_conversion_rates')
-
-    # rates are not yet populated
-    if conversion_rates.nil?
-      response['status'] = 'no data'
-      render json: response.to_json and return
+      response['error'] = 'invalid amount'
+      response['success'] = 'false'
+      render json: response.to_json, status: (:bad_request) and return
     end
 
-    # inputted currencies are not valid
+    # invalid currency
     if not (conversion_rates[from] and conversion_rates[to])
-      response['status'] = 'unsupported currency'
-      render json: response.to_json and return
+      response['error'] = 'unsupported currency'
+      response['success'] = 'false'
+      render json: response.to_json, status: (:bad_request) and return
     end
 
     from_rate = BigDecimal(conversion_rates[from].to_s)
     to_rate = BigDecimal(conversion_rates[to].to_s)
 
+    # do conversion
     Money.add_rate(from, to, to_rate/from_rate)
     result = Money.from_amount(amount, from).exchange_to(to)
 
-    response['result'] = result.format
-    response['result_raw'] = result.format(symbol: false, thousands_separator: false)
-    response['symbol'] = result.format(format: '%u')
-    response['last_update'] = Rails.cache.read('exchange_last_update').to_datetime
-    response['next_update'] = Rails.cache.read('exchange_next_update').to_datetime
-    response['status'] = 'success'
-    render json: response.to_json and return
+    response['success'] = 'true'
+
+    payload = {}
+    payload['result'] = result.format
+    payload['result_raw'] = result.format(symbol: false, thousands_separator: false)
+    payload['symbol'] = result.format(format: '%u')
+    payload['last_update'] = Rails.cache.read('exchange_last_update').to_datetime
+    payload['next_update'] = Rails.cache.read('exchange_next_update').to_datetime
+    response['payload'] = payload
+
+    render json: response.to_json, status: (:ok) and return
   end
 
+  # return raw rates
   def rates
-    # return raw rates
+    response = {}
     conversion_rates = Rails.cache.read('exchange_conversion_rates')
 
+    # rates are not yet populated
     if conversion_rates.nil?
-      response['status'] = 'no data'
-      render json: response.to_json and return
+      response['success'] = 'false'
+      response['error'] = 'no data'
+      render json: response.to_json, status: (:service_unavailable) and return
     end
+    response['success'] = 'true'
 
-    response['result'] = conversion_rates
-    response['last_update'] = Rails.cache.read('exchange_last_update').to_datetime
-    response['next_update'] = Rails.cache.read('exchange_next_update').to_datetime
-    response['status'] = 'success'
+    payload = {}
+    payload['result'] = conversion_rates
+    payload['last_update'] = Rails.cache.read('exchange_last_update').to_datetime
+    payload['next_update'] = Rails.cache.read('exchange_next_update').to_datetime
+    response['payload'] = payload
 
-    render json: response.to_json and return
+    render json: response.to_json, status: (:ok) and return
   end
+
+  def rootRoute
+    response = {}
+
+    response['success'] = 'true'
+
+    payload = {}
+    payload['example'] = request.original_url + '5.00/USD/CAD'
+    payload['usage'] = request.original_url + '{amount}/{currency_to_convert_from}/{currency_to_convert_to}'
+    response['payload'] = payload
+
+    render json: response.to_json, status: (:ok) and return
+  end
+
+  def badRoute
+    response = {}
+
+    response['success'] = 'false'
+    response['error'] = 'invalid path'
+
+    render json: response.to_json, status: (:bad_request) and return
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root "main#rootRoute"
   get "/:amount/:from_currency/:to_currency", to: "main#calculate", amount: /[^\/]+/, from_currency: /[^\/]+/, to_currency: /[^\/]+/
-  get '*path' => redirect('/')
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/rates", to: "main#rates"
+  get '*path', to: 'main#badRoute'
 end
