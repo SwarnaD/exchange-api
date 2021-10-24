f8a768fb8e1f586f957b078b778237516adaab6c
Swarnajyoti Datta <swarna.datta01@gmail.com>
2021-10-24 17:06:36 -0400

    deploy prep and readme update

--- README.md
+++ README.md
@@ -1,24 +1,78 @@
-# README
+# Exchange API
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+## Description
+A service to convert currencies.
 
-Things you may want to cover:
+## Usage 
 
-* Ruby version
+### Convert
+`GET https://guarded-earth-69779.herokuapp.com/<amount>/<currency_to_convert_from>/<currency_to_convert_to>`
 
-* System dependencies
+### Get Rates
+`GET https://guarded-earth-69779.herokuapp.com/rates`
 
-* Configuration
+## Examples
 
-* Database creation
 
-* Database initialization
+* `GET https://guarded-earth-69779.herokuapp.com/5/usd/cad`
 
-* How to run the test suite
+```json
+{
+    "success":"true",
+    "update": { 
+        "last_update":"2021-10-24T00:02:31.000+00:00", 
+        "next_update":"2021-10-25T00:33:01.000+00:00"
+    },
+    "payload": {
+        "result":"$7.38",
+        "result_raw":"7.38",
+        "symbol":"$"
+    }
+}
 
-* Services (job queues, cache servers, search engines, etc.)
+```
 
-* Deployment instructions
+* `GET https://guarded-earth-69779.herokuapp.com/20.57/gbp/JPY`
 
-* ...
+```json
+{
+  "success": "true",
+  "update": {
+    "last_update": "2021-10-24T00:02:31.000+00:00",
+    "next_update": "2021-10-25T00:34:21.000+00:00"
+  },
+  "payload": {
+    "result": "¥3,227",
+    "result_raw": "3227",
+    "symbol": "¥"
+  }
+}
+```
+
+* `GET https://guarded-earth-69779.herokuapp.com/rates`
+
+```json
+{
+  "success": "true",
+  "update": {
+    "last_update": "2021-10-24T00:02:31.000+00:00",
+    "next_update": "2021-10-25T00:33:01.000+00:00"
+  },
+  "payload": {
+    "result": {
+      "USD": 1,
+      "AED": 3.67,
+      "AFN": 84.95,
+      "ALL": 104.72,
+      "AMD": 477.87,
+      "ANG": 1.79,
+      "AOA": 603.48,
+      "ARS": 99.45,
+      "AUD": 1.34,
+      "AWG": 1.79,
+      "AZN": 1.7,
+      "...": "..."
+    }
+  }
+}
+
--- app/jobs/fetch_rates_job.rb
+++ app/jobs/fetch_rates_job.rb
@@ -8,19 +8,22 @@ class FetchRatesJob < ApplicationJob
   end
 
   def perform(*guests)
-    url = URI.parse(API_FULL_URL)
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port, use_ssl: true) {|http| http.request(req) }
-    failed_fetch = false
+    # fetch only if cache has no data OR an update is available
+    if (not Rails.cache.read('exchange_conversion_rates')) or (Rails.cache.read('next_update') <= Time.now)
+      url = URI.parse(API_FULL_URL)
+      req = Net::HTTP::Get.new(url.to_s)
+      res = Net::HTTP.start(url.host, url.port, use_ssl: true) {|http| http.request(req) }
+      failed_fetch = false
 
-    # save rates and update times to cache if call to API is successful
-    if res.is_a?(Net::HTTPSuccess) and (exchange_data = JSON.parse(res.body))['result'] == 'success'
-      Rails.cache.write('exchange_conversion_rates', exchange_data['rates'])
-      Rails.cache.write('exchange_next_update', Time.at(exchange_data['time_next_update_unix']).utc)
-      Rails.cache.write('exchange_last_update', Time.at(exchange_data['time_last_update_unix']).utc)
-    # retry a fetch relatively soon if request was unsuccessful
-    else
-      Rails.cache.write('exchange_next_update', Time.now.advance(seconds: SECONDS_TO_RETRY_FETCH))
+      # save rates and update times to cache if call to API is successful
+      if res.is_a?(Net::HTTPSuccess) and (exchange_data = JSON.parse(res.body))['result'] == 'success'
+        Rails.cache.write('exchange_conversion_rates', exchange_data['rates'])
+        Rails.cache.write('exchange_next_update', Time.at(exchange_data['time_next_update_unix']).utc)
+        Rails.cache.write('exchange_last_update', Time.at(exchange_data['time_last_update_unix']).utc)
+      # retry a fetch relatively soon if request was unsuccessful
+      else
+        Rails.cache.write('exchange_next_update', Time.now.advance(seconds: SECONDS_TO_RETRY_FETCH))
+      end
     end
   end
 end
